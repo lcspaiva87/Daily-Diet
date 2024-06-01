fec6631a212c5b621a0af92a71528cac8a1b07b8
paiva <lcspaiva87@gmail.com>
2024-06-01 20:14:25 -0300

    chore: Update npm dependencies and add new packages for tailwindcss customization

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,28 @@
 module.exports = {
   content: ['./src/**/*.tsx'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        alert: {
+          Dark: '#BF3B44',
+          Mid: '#F3BABD',
+          Light: '#F4E6E7',
+        },
+        sucessucess: {
+          Dark: '#639339',
+          Mid: '#CBE4B4',
+          Light: '#E5F0DB',
+        },
+        Base: {
+          100: '#1B1D1E',
+          200: '#333638',
+          300: '#5C6265',
+          400: '#828282',
+          500: '#B9BBBC',
+          600: '#333333',
+        },
+      },
+    },
   },
   plugins: [],
 }
-
