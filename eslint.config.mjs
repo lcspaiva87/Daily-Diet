28f53a4a6d9076deed04513eaee3f5c5b14ede36
paiva <lcspaiva87@gmail.com>
2024-06-01 19:15:30 -0300

    chore: Add configuration files and dependencies for linting and formatting

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import { fixupConfigRules } from '@eslint/compat'
+import pluginJs from '@eslint/js'
+import pluginReactConfig from 'eslint-plugin-react/configs/recommended.js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+// Desativar a regra 'react/react-in-jsx-scope'
+pluginReactConfig.rules = {
+  ...pluginReactConfig.rules,
+  'react/react-in-jsx-scope': 'off',
+  '@typescript-eslint/no-var-requires': 'off',
+  'no-undef': 'off',
+  'react/no-unescaped-entities': 'off',
+}
+
+export default [
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...fixupConfigRules(pluginReactConfig),
+]
